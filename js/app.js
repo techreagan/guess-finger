9cb584ae5bd9f6387e35660d25517442931ad4e4
Tech Reagan <reaganekhameye@gmail.com>
2020-09-06 18:29:57 +0100

    Add load event

--- js/app.js
+++ js/app.js
@@ -5,40 +5,39 @@ const UIfingers = document.querySelector('#fingers')
 
 let scores = 0
 let countDown = 20
-
-UIfingers.addEventListener('click', (e) => {
-  const btnFingerNum = e.target.parentElement.dataset.num
-  const randomNumber = Math.floor(Math.random() * 6)
-
-  if (!btnFingerNum) return
-
-  if (parseInt(btnFingerNum) === randomNumber) {
-    scores++
-    UIscore.innerText = scores
-
-    UIresultImg.setAttribute('src', `img/${randomNumber}.png`)
-    UIresultImg.parentElement.style.border = '4px solid green'
-    UIresultImg.parentElement.classList.add(
-      'animate__animated',
-      'animate__shakeY'
-    )
-    UIresultImg.parentElement.addEventListener('animationend', (e) => {
-      e.target.classList.remove('animate__animated', 'animate__shakeY')
-    })
-  } else {
-    UIresultImg.setAttribute('src', `img/${randomNumber}.png`)
-    UIresultImg.parentElement.style.border = '4px solid red'
-    UIresultImg.parentElement.classList.add(
-      'animate__animated',
-      'animate__shakeX'
-    )
-    UIresultImg.parentElement.addEventListener('animationend', (e) => {
-      e.target.classList.remove('animate__animated', 'animate__shakeX')
-    })
-  }
-})
-
 window.addEventListener('load', () => {
+  UIfingers.addEventListener('click', (e) => {
+    const btnFingerNum = e.target.parentElement.dataset.num
+    const randomNumber = Math.floor(Math.random() * 6)
+
+    if (!btnFingerNum) return
+
+    if (parseInt(btnFingerNum) === randomNumber) {
+      scores++
+      UIscore.innerText = scores
+
+      UIresultImg.setAttribute('src', `img/${randomNumber}.png`)
+      UIresultImg.parentElement.style.border = '4px solid green'
+      UIresultImg.parentElement.classList.add(
+        'animate__animated',
+        'animate__shakeY'
+      )
+      UIresultImg.parentElement.addEventListener('animationend', (e) => {
+        e.target.classList.remove('animate__animated', 'animate__shakeY')
+      })
+    } else {
+      UIresultImg.setAttribute('src', `img/${randomNumber}.png`)
+      UIresultImg.parentElement.style.border = '4px solid red'
+      UIresultImg.parentElement.classList.add(
+        'animate__animated',
+        'animate__shakeX'
+      )
+      UIresultImg.parentElement.addEventListener('animationend', (e) => {
+        e.target.classList.remove('animate__animated', 'animate__shakeX')
+      })
+    }
+  })
+
   let timer = setInterval(startCountDown, 1000)
 
   function startCountDown() {
